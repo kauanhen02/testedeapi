73c79966e4fdb6d2994fe8642fbaf8ade267155b
kauanhen02 <kauansjx31@gmail.com>
2024-02-27 10:51:36 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,42 @@
+
+const axios = require("axios");
+
+const url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.1/dados?formato=json";
+
+async function obterCotacaoDolarPTAXVenda() {
+  try {
+    const response = await axios.get(url);
+    const dados = response.data;
+    const dataAtual = new Date().toLocaleDateString();
+
+    let valorEncontrado = null;
+
+    for (let i = 0; i < dados.length; i++) {
+      if (dataAtual === dados[i].data) {
+        valorEncontrado = dados[i].valor;
+        break;
+      } 
+    }
+
+    for (let o = dados.length - 1; o >= 0; o--) {
+      if (dataAtual !== dados[o].data) {
+        valorEncontrado = dados[o].valor;
+        break;
+      } 
+    }
+    
+
+    if (valorEncontrado !== null) {
+      const resultado = { Valor: valorEncontrado };
+      const jsonResultado = JSON.stringify(resultado);
+      console.log(jsonResultado);
+    } else {
+      console.log(JSON.stringify({ error: "Cotação não encontrada para a data atual." }));
+    }
+  } catch (error) {
+    console.error("Erro ao obter a cotação:", error);
+  }
+}
+
+obterCotacaoDolarPTAXVenda();
+ 
